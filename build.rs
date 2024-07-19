0bce5efbf8901dcf58c3d95a831609bddb2e17cc
justtryingthingsout <31097061+justtryingthingsout@users.noreply.github.com>
2024-07-19 11:02:48 +0200

    Fixes for licence

--- build.rs
+++ build.rs
@@ -1,3 +1,21 @@
+/*
+    sepsplit-rs - A tool to split SEPOS firmware into its individual modules
+    Copyright (C) 2024 plzdonthaxme
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 extern crate bindgen;
 extern crate cc;
 
